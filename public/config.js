1f8fb6498eb9d65ba0dfd208e4ecbf7a40f5dad6
CB_X2_Jun <zzh20120518@outlook.com>
2025-04-12 12:47:13 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -8,12 +8,14 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'm798505280-ae627d1c53c5f42fee173fd4',
+    'm798522668-507502ed6be1465d4f6090ed',
+    'm798505242-325e0fd1ee9bdc49622d6cc9',
   ],
 
     // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 90,
 
   // 是否显示检测站点的链接
   ShowLink: true,
