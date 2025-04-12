32068268b08f20967e4477996189c50ac50666c1
CB_X2_Jun <zzh20120518@outlook.com>
2025-04-12 21:15:22 +0800

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -25,7 +25,7 @@ function App() {
         <div id='footer'>
           <p>基于&nbsp;<Link to='https://uptimerobot.com/' text='UptimeRobot' />&nbsp;接口&nbsp;|&nbsp;检测频率：5&nbsp;分钟</p>
           <p><a href="https://icp.gov.moe/?keyword=20255955" target="_blank">萌ICP备20255955号</a></p>
-          <p><a href="https://app.netlify.com/sites/cb-stats/deploys" target="_blank"><img src="https://api.netlify.com/api/v1/badges/2c80c2ef-d5d8-46b3-bb48-d4548eb71f67/deploy-status" alt="由 Netlify 部署" width="118" height="20" /></a>
+          <p><a href="https://app.netlify.com/sites/cb-stats/deploys" target="_blank"><img src="https://api.netlify.com/api/v1/badges/2c80c2ef-d5d8-46b3-bb48-d4548eb71f67/deploy-status" alt="由 Netlify 部署" width="118" height="20" /></a></p>
         </div>
       </div>
     </>
